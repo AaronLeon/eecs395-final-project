f57ed70bcc6a2c880453fb6adc00e409613f0d62
yulunwu1 <yulunwu@dhcp-10-105-113-6.wireless.northwestern.private>
2017-04-10 21:40:45 -0500

    register implemented, doubles penalty implemented, movesPossible wip

--- Parcheesi.java
+++ Parcheesi.java
@@ -9,7 +9,9 @@ import java.lang.Math;
 
 public class Parcheesi implements Game {
     private Board board;
+    private int registered=0;
     private Player[] players = new Player[4];
+    colors=["blue","yellow","green","red"];
 
 
     public Parcheesi() {
@@ -52,7 +54,14 @@ public class Parcheesi implements Game {
     }
 
     public void register(Player p) {
-
+        if(registered==4){
+            return;
+        } else {
+            Player newPlayer=newPlayer();
+            newPlayer.startGame(colors[registered])
+            players[registered]=newPlayer;
+            registered++;
+        }
     }
 
     public void start() {
@@ -107,16 +116,56 @@ public class Parcheesi implements Game {
     }
 
     public void doublesPenalty(Player p) {
-        Pawn furthestPawn=null;
-        //now we iterate over
+        int furthestPawn=0;
+        String color = p.color;
+        int home_index=board.homeLocations[color];
+        int dist=0;
+        int furthestDistance=-1;
+        for (int i = 0; i < 4; i++){
+            Pawn curr = p.pawns[i];
+            if (curr.location==-1){
+                continue;
+            } else{
+                //piece not at home
+                dist=curr.location-home_index;
+                if (curr.location>home_index){
+                    if (dist>furthestDistance){
+                        furthestDistance=dist;
+                        furthestPawn=i;
+                    }
+                } else{
+                    dist=68-dist;
+                    if (dist>furthestDistance){
+                        furthestDistance=dist;
+                        furthestPawn=i;
+                    }
+                }
+            }
+        }
     }
 
     public void cheat(int i) {
+        //also remove player's pieces from the board
+        //write a for loop
         players[i] = null;
     }
 
     public boolean movesPossible(Player p, int[] dice, Board board) {
-        return true;
+        //iterate over pawns in player p,
+        for (int i=0;i<4;i++){
+            //iterate over rolls in dice,
+            for(int j = 0;j<dice.length;j++){
+                // generate move case wise:
+                // enterPiece
+                // moveMain
+                // moveHome
+
+                // check if move isBlockated
+
+                //
+            }
+        }
+        return false;
     }
 
     public boolean movedBlockade(Player before, Player after) {
