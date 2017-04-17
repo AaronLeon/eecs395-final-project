78b2c20e8cd3e1d48bd54b38650e03808b318b35
yulunwu1 <yulunwu@dhcp-10-105-70-104.wireless.northwestern.private>
2017-04-16 22:46:39 -0500

    bop

--- Board.java
+++ Board.java
@@ -49,75 +49,74 @@ public class Board {
 
     public int bopLoc(int location, String color) {
         //assume no blockade
-        boolean clear1=ring[location].first==null;
-        boolean clear2=ring[location].second==null;
+        boolean clear1 = ring[location].first == null;
+        boolean clear2 = ring[location].second == null;
 
-        if(clear1&& ring[location].second.color!=color){
+        if (clear1 && ring[location].second.color != color) {
             return 1;
         }
-        if(clear2&& ring[location].first.color!=color){
+        if (clear2 && ring[location].first.color != color) {
             return 0;
         }
         return -1;
         //checks if inserting color at loc will bop
     }
 
-    public void removeIndex(int location, int index){
-        if(index==0){
-            ring[location].first=null;
-        } else if (index==1) {
-            ring[location].second=null;
+    public void removeIndex(int location, int index) {
+        if (index == 0) {
+            ring[location].first = null;
+        } else if (index == 1) {
+            ring[location].second = null;
         }
     }
 
     public boolean add(int location, String color, int id) {
         //bops if needed
-        boolean clear1=ring[location].first==null;
-        boolean clear2=ring[location].second==null;
+        boolean clear1 = ring[location].first == null;
+        boolean clear2 = ring[location].second == null;
 
-        if (clear1&&clear2) {
-            ring[location].first=new Pawn(id,color);
-        }
-        else if (!clear1){
-            ring[location].second=new Pawn(id,color);
-            }
-        else {
-            ring[location].first=new Pawn(id,color);
+        if (clear1 && clear2) {
+            ring[location].first = new Pawn(id, color);
+        } else if (!clear1) {
+            ring[location].second = new Pawn(id, color);
+        } else {
+            ring[location].first = new Pawn(id, color);
         }
 
         return false;
     }
-}
 
-public class Home {
-    private String color;
-    private LinkedList<Pawn> home;
 
-    public Home(String color) {
-        this.color = color;
+    public class Home {
+        private String color;
+        private LinkedList<Pawn> home;
 
-        home = new LinkedList<>();
-        for (int i = 0; i < 4; i++) {
-            home.add(new Pawn(i, color));
+        public Home(String color) {
+            this.color = color;
+
+            home = new LinkedList<>();
+            for (int i = 0; i < 4; i++) {
+                home.add(new Pawn(i, color));
+            }
         }
     }
-}
 
-public class Runway {
-    private String color;
-    private int pieces;
-    private Pawn[][] runway = new Pawn[7][2];
+    public class Runway {
+        private String color;
+        private int pieces;
+        private Pawn[][] runway = new Pawn[7][2];
 
-    public Runway(String color) {
-        this.color = color;
-        pieces = 0;
-    }
+        public Runway(String color) {
+            this.color = color;
+            pieces = 0;
+        }
 
-    public boolean empty(int i) {
-        return runway[i][0] == null && runway[i][1] == null;
-    }
+        public boolean empty(int i) {
+            return runway[i][0] == null && runway[i][1] == null;
+        }
 
-    public boolean blocked(int i) {
-        return runway[i][0] != null && runway[i][1] != null;
+        public boolean blocked(int i) {
+            return runway[i][0] != null && runway[i][1] != null;
+        }
     }
 }
